786fe8eec53c880f0151fc2dbff901db17c0b844
Quzay <qwertyn3146@gmail.com>
2026-03-18 21:32:36 +0200

    register facebook to oauth

--- app/model/base.py
+++ app/model/base.py
@@ -2,9 +2,11 @@ from sqlalchemy.orm import DeclarativeBase
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from authlib.integrations.flask_client import OAuth
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 jwt = JWTManager()
+oauth = OAuth()
