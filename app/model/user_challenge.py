50ccf406e9b504cc5cd08f9b5a586f9128b92a47
Quzay <qwertyn3146@gmail.com>
2026-03-26 18:48:08 +0200

    adding new derivative attribute

--- app/model/user_challenge.py
+++ app/model/user_challenge.py
@@ -1,9 +1,11 @@
 import datetime
 from .base import db 
-from sqlalchemy.orm import mapped_column,Mapped, relationship , validates
-from sqlalchemy import String,Date , Enum , ForeignKey
+from sqlalchemy.orm import mapped_column,Mapped, relationship , validates , column_property
+from sqlalchemy import Date , Enum , ForeignKey , select, func
 from typing import Optional,List
 from .enums import Challenge_status
+from .activity import Activity 
+from .challenge import Challenge
 
 class User_challenge(db.Model):
     __tablename__ = "user_challenge"
@@ -17,3 +19,12 @@ class User_challenge(db.Model):
 
     challenge: Mapped[List["Challenge"]] = relationship(back_populates="user_challenges")
     user: Mapped[List["User"]] = relationship(back_populates="user_challenges")
+
+    current_value:Mapped[int] = column_property(
+        select(func.coalesce(func.sum(Activity.distance_meters), 0))
+       .join(Challenge, Challenge.id == challenge_id)
+       .where(Activity.user_id == user_id , func.date(Activity.day) >= Challenge.created_at)
+       .scalar_subquery()
+        )
+           
+    
