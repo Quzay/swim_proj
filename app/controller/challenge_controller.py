aaf6a11a268940c75c71bec41a19b834711471f9
Quzay <qwertyn3146@gmail.com>
2026-03-30 18:56:31 +0300

    removing unused endpoints and show_all_challenges

--- app/controller/challenge_controller.py
+++ app/controller/challenge_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from app.model import db , Challenge, User 
 from flask_jwt_extended import jwt_required, get_jwt , get_jwt_identity
 from sqlalchemy.exc import IntegrityError 
+from sqlalchemy import select
 
 challenge_bp = Blueprint("challenge" , __name__)
 
@@ -22,35 +23,24 @@ def create_challenge(competition_id):
     db.session.commit()
     return jsonify({"message":"Challenge was successful created"}) , 201
 
-@challenge_bp.post("/<int:challenge_id>")
-@jwt_required()
-def take_part_in_challenge(challenge_id):
-    email = get_jwt_identity()
-    user = User.query.filter_by(email = email).first()
-    if not user:
-        return jsonify({"message":"Please log in"}) , 401
-    user_in_challenge = User_challenge(
-        user_id = user.id,
-        challenge_id = challenge_id,
-        status = "ACTIVE"
-    )
-    db.session.add(user_in_challenge)
-    db.session.commit()
-    return jsonify({"message":"You take part in challenge"}) , 201
 
-@challenge_bp.get("/") # Need to change endpoint
+@challenge_bp.get("/competition/<int:competition_id>/challenges/") 
 @jwt_required()
-def show_users_active_challenge():
-    email = get_jwt_identity()
-    user = User.query.filter_by(email = email).first()
+def show_all_challenges(competition_id):
+    user = User.find_by_email(get_jwt_identity()) # Чи треба взагалі?
     if not user:
         return jsonify({"message":"Please log in"}) , 401
-    active_challenge = User_challenge.query.filter_by(user_id = user.id).first()
-    return jsonify({
-        "id" : active_challenge.id,
-        "user_id" : active_challenge.user_id,
-        "challenge_id" : active_challenge.challenge_id,
-        "status" : active_challenge.status,
-        "current_value" : active_challenge.current_value,
-        "compleated_at" : active_challenge.compleated_at
-        }) , 201
+    stmt = select(Challenge).where(Challenge.competition_id == competition_id)
+    page = request.args.get("page", default=1, type=int)
+    per_page = request.args.get("per_page" , default=3, type=int)
+    pagination = db.paginate(stmt, page = page,per_page= per_page, error_out=False)
+    res = []
+    for challenge in pagination:
+        res.append({
+            "id" : challenge.id,
+            "name" : challenge.name,
+            "description": challenge.description,
+            "distance": challenge.distance,
+            "competition_id" : challenge.competition_id
+        })
+    return jsonify(res), 200
