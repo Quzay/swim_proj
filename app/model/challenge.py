0d01b3d4e7486a70366ba103cf7d040410142feb
Quzay <qwertyn3146@gmail.com>
2026-04-05 10:50:05 +0300

    add cascade delete to achievements in Challenge and challenges in Competition

--- app/model/challenge.py
+++ app/model/challenge.py
@@ -1,8 +1,9 @@
 import datetime
 from .base import db
 from sqlalchemy.orm import mapped_column,Mapped, relationship , validates 
-from sqlalchemy import String,DateTime ,Text , ForeignKey , func
+from sqlalchemy import String,DateTime ,Text , ForeignKey , func 
 from typing import Optional,List
+from flask import jsonify
 
 class Challenge(db.Model):
     __tablename__ = "challenge"
@@ -12,15 +13,20 @@ class Challenge(db.Model):
     description:Mapped[Optional[str]] = mapped_column(String(110))
     distance:Mapped[int] = mapped_column()
 
-    achievements:Mapped[List["Achievement"]] = relationship(back_populates="challenge")
+    achievements:Mapped[List["Achievement"]] = relationship(back_populates="challenge", cascade="all, delete-orphan")
 
     competition_id:Mapped[int] = mapped_column(ForeignKey("competition.id"))
     competitions: Mapped["Competition"] = relationship(back_populates="challenges")
     
-    __table_args__ = (
-        db.CheckConstraint('expired_at >= CURRENT_DATE' , name = "ck_challange"),
-    )
+    # __table_args__ = (
+    #     db.CheckConstraint('expired_at >= CURRENT_DATE' , name = "ck_challange"),
+    # )
 
-    def get_challenge_by_id(challenge_id):
-        return Challenge.query.get(challenge_id)
+    @classmethod
+    def get_by_id(cls,challenge_id):
+        return db.session.get(cls,challenge_id)
     
+    def not_in_competition(self, competition_id):
+        if self.competition_id != competition_id:
+            return True
+        return False
